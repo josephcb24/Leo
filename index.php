a9792254e30de4146651afafa1cdf8819215885e
josephcb24 <JCBizzle@Josephs-MacBook-Pro.local>
2015-07-23 18:19:03 -0400

    Actual Tutu and Leo Site
    Dance Costumes Seller

---
+++ index.php
@@ -0,0 +1,11 @@
+Dear our lovely TokoPress buyer,
+<p>
+Please don not upload "COMPLETE PACKAGE" ZIP file directly to your Wordpress Dashboard.
+<p>
+Please extract the ZIP file first and read the documentation first.
+<p>
+Thank you very much.
+<p>
+Regards,
+<br>
+TokoPress
